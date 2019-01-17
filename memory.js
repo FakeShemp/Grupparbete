c507a538451fde51b6dc862414b9b1a68af4c926
schoolecutbildning <42963100+schoolecutbildning@users.noreply.github.com>
2019-01-17 12:08:08 +0100

    Updating various bits of code/comments

--- memory.js
+++ memory.js
@@ -1,20 +1,24 @@
 
-// Starts the Memory game. The eventhandler is on the #memory element.
+// Starts the Memory game. The eventhandler is on the mapped SVG, on the #memory element.
+// Defines global variable itemChoice. If the player said "yes" to taking the orb, the player
+// has a choice to change the face of one of the cards.
+
+var itemChoice;
+console.log("My page: " + itemChoice);
 
 function startGame() {
 
     var answer;
 
-    //var itemChoice;
-    console.log("My page: " + itemChoiceG);
-    
-    if (itemChoiceG == "yes") {
-        var answer;
+// Depending on what the player chooses (yes or no), the face of one card changes depending
+// on what they choose in the following function.    
+    if (itemChoice == "yes") {
         while (answer != "rose" && answer != "skull" && answer != "dragon") {
             var answer = prompt("You come across dusty old cards. One of them is blank. Which image should it have? (Choose: skull, rose, dragon)");
             console.log(answer)
         }
-
+// If the player chose a skull or a rose or a dragon, one of the cards changes via the following
+// if statement.
         if (answer == "skull") {
             document.getElementById('wildskull').style.display = "block";
         } else if (answer == "rose") {
@@ -23,9 +27,11 @@ function startGame() {
             document.getElementById('wilddragon').style.display = "block";
         }
      }
-
+// The game loads.
     document.getElementById('game').style.display = "flex";
     document.getElementById('fade').style.display = "block";
+    document.getElementById('wildrose').style.display = "block";
+
 };
 
 // Selects the cards and sets the default states for cards and board.
@@ -60,13 +66,14 @@ function flipCard() {
 // If the cards are NOT a match, invokes unflipCards function. 
 // If they ARE a match, invokes disableCards function.
 // Each time cards are flipped, the complete function is invoked to see if all matches have been made.
+// This counts as a player move and all player moves are recorded.
 
 var playerMoves = 0;
 
 function checkForMatch() {
 
     let isMatch = cardOne.dataset.name === cardTwo.dataset.name;
-    // isMatch ? disableCards() : unflipCards();
+
     if (isMatch == true) {
         disableCards();
         playerCount();
@@ -91,6 +98,8 @@ function disableCards() {
     resetBoard();
 }
 
+// The cards are unflipped, classes are removed and the resetBoard function is invoked.
+
 function unflipCards() {
     setTimeout(() => {
         cardOne.classList.remove('flip');
@@ -100,12 +109,16 @@ function unflipCards() {
     }, 1500);
 }
 
+// Resets the board.
+
 function resetBoard() {
     flippedCard = false;
     locked = false;
     cardOne = null;
     cardTwo = null;
 }
+
+// Shuffles the cards at the start of the game.
 
 (function shuffle() {
     cards.forEach(card => {
@@ -114,8 +127,13 @@ function resetBoard() {
     });
 })();
 
+// Adds event listeners to cards. Click and mouseenter are used.
+
 cards.forEach(card => card.addEventListener('click', flipCard));
 cards.forEach(card => card.addEventListener('mouseenter', soundEffect));
+
+// This function records all matches. Every time a match is made, showFinishPopup is invoked and
+// is sent the number of matches made.
 
 function completeGame() {
     let match = 0;
@@ -129,18 +147,23 @@ function completeGame() {
     )
 };
 
+// There are only 8 possible matches. Once 8 matches are made, the rest of the function works.
+// See comments inside the function.
+
 function showFinishPopup(match) {
 
     // Handles Matching
     if (match == 8) {
         setTimeout(function () {
-
+            // Shows the win popup and fades out the background. The gold key function is invoked.
             document.getElementById('win').style.display = "block";
             document.getElementById('winfade').style.display = "block";
             addGoldKey();
 
+            // When the okay button is clicked, the various windows are hidden, this removing the game.
             document.getElementById('okay').onclick = function () {
 
+                // The memory onclick node is removed to prevent the player from playing again.
                 document.getElementById("memory").outerHTML = "";
 
                 let removeGame = document.getElementsByClassName("phase2");
@@ -154,19 +177,22 @@ function showFinishPopup(match) {
     };
 }
 
+// Global variable for playerMoves, which tallies at the end of the game.
 var playerMoves = 0;
 
 function playerCount() {
     playerMoves++;
     console.log("Player moves: " + playerMoves);
 
-
+// Checks to see if the win popup is visible. If it is, playerMoves is checked and the player
+// is awarded a gold/silver/bronze star depending on the criteria.
     let gameFinished = document.getElementById("win");
     setTimeout(function () {
         if (gameFinished.style.display === 'block') {
 
             document.getElementById('moves').style.display = "block";
 
+            // Handles showing the star and the appropriate message.
             switch (true) {
                 case (playerMoves <= 8):
                     document.getElementById('goldstar').style.display = "block";
@@ -189,7 +215,7 @@ function playerCount() {
     }, 1020);
 }
 
-// Mouseenter Effect
+// Mouseenter Effect: Plays a soft sound.
 
 function soundEffect() {
     var soundEffectHover = new Audio('ding-3.wav');
@@ -204,7 +230,7 @@ function soundEffect() {
 var playerKeys = [];
 var keySuccess = ["gold key", "red key", "purple key", "pink key"];
 
-// Key from Memory game function.
+// Add the gold from winning the Memory game.
 
 function addGoldKey() {
     let goldKey = "gold key";
@@ -214,6 +240,8 @@ function addGoldKey() {
 
 // Handles checking if the player has all the keys in order to open the door.
 
+var playerName;
+
 function checkKeys() {
 
     var success = keySuccess.every(function (val) {
@@ -221,9 +249,9 @@ function checkKeys() {
     });
 
     if (success == true) {
-        alert("you win!")
+        alert("The door opens with a creak and you run out. " +playerName+ ", you escaped the room!")
     } else {
-        alert("you need more keys. you have the following keys: " + playerKeys.join(", ") + ".")
+        alert("You need more keys, " +playerName + ". You have the following keys: " + playerKeys.join(", ") + ".")
     }
 
 };
